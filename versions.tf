355d4e9a3b32a196c65c6d9de1970d854aed2493
root <root@ip-172-31-82-239.ec2.internal>
2025-07-22 07:28:07 +0000

    updated version

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
-
 terraform {
   required_version = ">= 0.12"
+
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -8,11 +8,11 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = ">= 2.7.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = ">= 3.68.0, < 6.0.0"
     }
     local = {
       source  = "hashicorp/local"
